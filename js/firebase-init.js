5a7b48e7120e88e9f6cbf62838ea07e44204f18e
multi-inversiones-de-occidente <boxthe028@gmail.com>
2025-05-16 06:59:44 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,20 +1,14 @@
 // js/firebase-init.js
-
-// 1) Configuración EXACTA de Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyBWEXs6RIB_LJUdbPdYRGh2ARZmpHhupMo",
   authDomain: "multinversiones-38e13.firebaseapp.com",
   projectId: "multinversiones-38e13",
-  storageBucket: "multinversiones-38e13.appspot.com",  // fíjate que termine en .appspot.com
+  storageBucket: "multinversiones-38e13.appspot.com",
   messagingSenderId: "380001917078",
   appId: "1:380001917078:web:b5eccc64bc77709bd2cab7",
   measurementId: "G-H4E31XHG1Y"
 };
-
-// 2) Inicializa Firebase “compat”
 firebase.initializeApp(firebaseConfig);
-
-// 3) Expón globalmente los servicios
 window.auth      = firebase.auth();
 window.db        = firebase.firestore();
 window.messaging = firebase.messaging();

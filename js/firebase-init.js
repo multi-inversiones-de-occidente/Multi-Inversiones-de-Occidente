ab64c96b70d3ab7c6b4639704cc3fcd3f96a3826
multi-inversiones-de-occidente <boxthe028@gmail.com>
2025-05-16 07:13:11 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,4 +1,5 @@
 // js/firebase-init.js
+
 const firebaseConfig = {
   apiKey: "AIzaSyBWEXs6RIB_LJUdbPdYRGh2ARZmpHhupMo",
   authDomain: "multinversiones-38e13.firebaseapp.com",
@@ -8,7 +9,9 @@ const firebaseConfig = {
   appId: "1:380001917078:web:b5eccc64bc77709bd2cab7",
   measurementId: "G-H4E31XHG1Y"
 };
+
 firebase.initializeApp(firebaseConfig);
+
 window.auth      = firebase.auth();
 window.db        = firebase.firestore();
 window.messaging = firebase.messaging();
